c88ca0d8693b4b446c3527b52d529e7c0925ab54
trixie3 <trixie3669@gmail.com>
2023-04-24 21:17:05 +0100

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+  *main - Entry point
+  *
+  * A program using standard library
+  *
+  *Return - Always 0 (sucess)
+  *
+  */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
