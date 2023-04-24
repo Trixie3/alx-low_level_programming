6cf613bb5040865a138e7fc9806412849da2210b
trixie3 <trixie3669@gmail.com>
2023-04-24 21:36:54 +0100

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+  *main - Entry point
+  *
+  *A programme that prints standard output
+  *
+  *
+  *Return: Always 0 (sucess)
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+	return (0);
+
+}
