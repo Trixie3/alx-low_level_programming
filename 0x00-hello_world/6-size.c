159fa9a4654050aab4d60ce083dfc5a9aa970981
trixie3 <trixie3669@gmail.com>
2023-04-24 23:02:36 +0100

    final task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
 
-	printf("Size of an long int: %li byte(s)\n", sizeof(int));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
 
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 
