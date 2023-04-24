2f4326539534a5d97309f48e3ffbc6d273222f2b
trixie3 <trixie3669@gmail.com>
2023-04-25 00:47:13 +0100

    final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+/**
+  *main - Entry point
+  *
+  *
+  * A program using a pointer
+  *
+  *Return: Always 1 (standard error)
+  *
+  */
+int main(void)
+{
+	char *a = "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", a);
+
+
+	return (1);
+}
