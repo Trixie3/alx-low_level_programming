19b6e72041e33eaadb45e702a98c3f7484342477
trixie <trixie3669@gmail.com>
2023-09-08 07:51:10 -0700

    variables in c scripts

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Use `putchar` to print
+ * lowercase and then uppercase alphabet.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
