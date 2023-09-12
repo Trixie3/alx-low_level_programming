ec84db3b99fd39ea82c7e603f3f61a91ca6c2408
trixie <trixie3669@gmail.com>
2023-09-12 15:49:26 -0700

    nested functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return 0
+  */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+												}
+	_putchar('\n');
+	}
+}
+
+
