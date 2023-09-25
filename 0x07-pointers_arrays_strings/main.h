56753bce82db8e58bbbc00f387e9dfe1473ab7e6
trixie <trixie3669@gmail.com>
2023-09-25 15:44:27 -0700

    pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,17 @@
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int _putchar(char);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-int _putchar(char c);
-void set_string(char **s, char *to);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
+
+
+#endif
